9226f8ef0b6ee5f89b82ec02c9a58c2cc7d734fe
Walter Moreira <walter@waltermoreira.net>
2025-12-01 18:48:20 -0600

    Attempts to make .git deterministic

--- flake.nix
+++ flake.nix
@@ -98,6 +98,157 @@
             go-task
           ] ++ lib.optional stdenv.isDarwin apple-sdk_14;
         };
+        mathlib = leanVersion:
+          let
+            hashes = {
+              aarch64-darwin = {
+                "4.20.1" = "sha256-Bok3lictsT3gsNsV2sSnqb9UYWxPa+R+UI25U1UXt/0=";
+                "4.21.0" = "";
+                "4.22.0" = "";
+              };
+              aarch64-linux = {
+                "4.20.1" = "";
+                "4.21.0" = "";
+                "4.22.0" = "";
+              };
+              x86_64-darwin = {
+                "4.20.1" = "";
+                "4.21.0" = "";
+                "4.22.0" = "";
+              };
+              x86_64-linux = {
+                "4.20.1" = "";
+                "4.21.0" = "";
+                "4.22.0" = "";
+              };
+            };
+            lean = toolchain leanVersion;
+          in
+          pkgs.stdenv.mkDerivation {
+            name = "mathlib-${leanVersion}";
+            outputHashAlgo = "sha256";
+            outputHashMode = "recursive";
+            outputHash = hashes.${system}.${leanVersion};
+            src = ./empty;
+            buildInputs = with pkgs; [
+              lean
+              rsync
+              curl
+              git
+              coreutils
+              moreutils
+              ripgrep
+              gnused
+              jq
+              findutils
+            ];
+            nativeBuildInputs = with pkgs; [
+              cacert
+            ];
+            phases = [ "unpackPhase" "buildPhase" ];
+            buildPhase = ''
+              substituteInPlace lakefile.toml lean-toolchain \
+                --subst-var-by leanVersion "${leanVersion}"
+              mkdir -p $out
+              export HOME=$(mktemp -d)
+              git config --global user.name "No Name"
+              git config --global user.email "<no@email.org>"
+              lake exe cache get
+              cd .lake/packages
+              # Remove all git hooks
+              echo "----- Removing hooks"
+              find . -regex '.*\.git/hooks\($\|/.*\)' -delete
+              # Remove local data from git refs
+              echo "----- Cleaning up refs"
+              for f in $(find . -path "*.git/logs/*" -type f); do
+                sed -E -i -e 's/[^[:space:]]+/0000000000/6' "$f"
+              done
+              # Clear trace files from local info
+              echo "----- Cleaning up traces"
+              for f in $(find . -path "*.lake*.trace" -type f); do
+                jq '.log[]?.message = ""' "$f" | sponge "$f"
+                jq '.inputs = []' "$f" | sponge "$f"
+              done
+              # rg 'nix/store' .lake/packages || true
+              # tar zcf $out/foo.tgz .
+              # TODO: remove .git/hooks
+              # TODO: zero out the date and emails in .git/logs
+              # TODO: zero out paths in trace files
+              rsync -av . $out
+              # rsync -av --include="*/" --include="*.olean" --exclude="*" \
+              #   .lake/packages/ $out
+            '';
+          };
+        test = pkgs.fetchgit {
+          name = "test";
+          url = "https://github.com/waltermoreira/nginx-test.git";
+          leaveDotGit = true;
+          rev = "104f8596ad7d13305ce8d4cd894d4f9a59bc0aea";
+          hash = "sha256-EEG/6zfF9PsZ4cF/72Thnp6AwMoAGDp6PlYgLC0HQRA=";
+        };
+        mathlibHashes = {
+          aesop = {
+            rev = "ddfca7829bf8aa4083cdf9633935dddbb28b7b2a";
+            hash = "sha256-4j2VuLmlGCmscGpb6hrDwBFUwjBOtUchGSzCQGpSQv8=";
+            url = "https://github.com/leanprover-community/aesop";
+          };
+          batteries = {
+            rev = "7a0d63fbf8fd350e891868a06d9927efa545ac1e";
+            hash = "sha256-LyiG6rc9kNn8K0/2mDaD8RtOsTwK9vYQHiffNXNi/VU=";
+            url = "https://github.com/leanprover-community/batteries";
+          };
+          Cli = {
+            rev = "f9e25dcbed001489c53bceeb1f1d50bbaf7451d4";
+            hash = "sha256-6VZQ4v4bTCuDy0JZNIM0ckw4b5gyHvQmhT/4Keoq0xE=";
+            url = "https://github.com/leanprover/lean4-cli";
+          };
+          importGraph = {
+            rev = "a11bcb5238149ae5d8a0aa5e2f8eddf8a3a9b27d";
+            hash = "sha256-tOMps125FAUTZxZyzPt3VVQpg3f5IPjabTYq8JLcr3o=";
+            url = "https://github.com/leanprover-community/import-graph";
+          };
+          LeanSearchClient = {
+            rev = "6c62474116f525d2814f0157bb468bf3a4f9f120";
+            hash = "sha256-HLw4aMWH5UHqhbmc42fi7/jxlSEcqs0oE99ANjwfmog=";
+            url = "https://github.com/leanprover-community/LeanSearchClient";
+          };
+          mathlib = {
+            rev = "5c0c94b3f563ed756b48b9439788c53b0d56a897";
+            hash = "sha256-joY0g+ZXhRP6buFL3X/37qMtZ7ep6N/e3p3VTLdYTtY=";
+            url = "https://github.com/leanprover-community/mathlib4";
+          };
+          plausible = {
+            rev = "2ac43674e92a695e96caac19f4002b25434636da";
+            hash = "sha256-5NY4ewpgno+O28xvhHbB2jjX45uvQtisSd8zQx0hR58=";
+            url = "https://github.com/leanprover-community/plausible";
+          };
+          proofwidgets = {
+            rev = "21e6a0522cd2ae6cf88e9da99a1dd010408ab306";
+            hash = "sha256-lGCDKbVSh0W/MLFW9k5WrQP/pk5lDP2dwOI25x9La24=";
+            url = "https://github.com/leanprover-community/ProofWidgets4";
+          };
+          Qq = {
+            rev = "2865ea099ab1dd8d6fc93381d77a4ac87a85527a";
+            hash = "sha256-hIdwZC0HGNeyRs9KPszvLAUZcRTuza8OGITxMtt9//w=";
+            url = "https://github.com/leanprover-community/quote4";
+          };
+        };
+        mathlibRepos = pkgs.linkFarm "mathlibRepos" (
+          builtins.attrValues
+            (builtins.mapAttrs
+              (name: value: {
+                inherit name;
+                path = pkgs.fetchgit {
+                  inherit name;
+                  url = value.url;
+                  leaveDotGit = true;
+                  rev = value.rev;
+                  hash = value.hash;
+                };
+              })
+              mathlibHashes
+            )
+        );
       in
       {
         packages = {
@@ -105,6 +256,8 @@
           lean-toolchain-4_21 = toolchain "4.21.0";
           lean-toolchain-4_22 = toolchain "4.22.0";
           default = toolchain "4.22.0";
+          mathlib-4_20 = mathlib "4.20.1";
+          inherit test mathlibRepos;
         };
 
         devShells = {
