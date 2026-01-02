d1ec131e2066f10e5eebf3f9a96030bcb39eaeb5
Walter Moreira <walter@waltermoreira.net>
2026-01-02 15:36:48 -0600

    add hash for linux x86

--- flake.nix
+++ flake.nix
@@ -184,7 +184,7 @@
                 "4.22.0" = "";
               };
               x86_64-linux = {
-                "4.20.1" = "";
+                "4.20.1" = "sha256-htydXQd30riiTc4ZKd+aDHMLMZ4LSEj9cywwgY4GHNw=";
                 "4.21.0" = "";
                 "4.22.0" = "";
               };
